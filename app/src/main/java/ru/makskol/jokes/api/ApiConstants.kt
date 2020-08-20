9490bd08cab272604de891565dffe4868fc20ed4
Nobetrayal <thelordofhell@mail.ru>
2020-08-20 16:31:24 +0300

    Extracted some string constants

---
+++ app/src/main/java/ru/makskol/jokes/api/ApiConstants.kt
@@ -0,0 +1,16 @@
+package ru.makskol.jokes.api
+
+class ApiConstants {
+
+    companion object {
+
+        const val CHARACTER_NAME = "Chuck"
+        const val CHARACTER_LAST_NAME = "Norris"
+
+        const val PARAMETER_FIRST_NAME = "firstName"
+        const val PARAMETER_lAST_NAME = "lastName"
+        const val PARAMETER_COUNT = "count"
+
+    }
+
+}
--- app/src/main/java/ru/makskol/jokes/api/ApiService.kt
+++ app/src/main/java/ru/makskol/jokes/api/ApiService.kt
@@ -4,14 +4,13 @@ import io.reactivex.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-import ru.makskol.jokes.pojo.Joke
 import ru.makskol.jokes.pojo.JokesResponse
 
 interface ApiService {
 
     @GET("random/{count}")
-    fun getJokes(@Path("count") count: String,
-                 @Query("firstName") firstName: String = "Chuck",
-                 @Query("lastName") lastName: String = "Norris"): Single<JokesResponse>
+    fun getJokes(@Path(ApiConstants.PARAMETER_COUNT) count: String,
+                 @Query(ApiConstants.PARAMETER_FIRST_NAME) firstName: String = ApiConstants.CHARACTER_NAME,
+                 @Query(ApiConstants.PARAMETER_lAST_NAME) lastName: String = ApiConstants.CHARACTER_LAST_NAME): Single<JokesResponse>
 
 }
